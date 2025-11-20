7e26b392dd6de56300bfb11024999e891fe7ec70
brokenstcg <brokenstcg@gmail.com>
2025-11-19 22:04:30 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures relative paths for flexible hosting (GitHub Pages etc.)
+  base: './broken-tcg/', // Ensures relative paths for flexible hosting (GitHub Pages etc.)
 })
